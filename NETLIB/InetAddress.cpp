3f1af405397976cbc2ecf4941dfe0d9de9f231b2
youryuu <l9819y@gmail.com>
2022-02-28 16:22:19 +0800

    InetAddress

--- NETLIB/InetAddress.cpp
+++ NETLIB/InetAddress.cpp
@@ -18,8 +18,8 @@ InetAddress::InetAddress(uint16_t port)
     memset(&addr_, 0, sizeof addr_);
     addr_.sin_family = AF_INET;
     in_addr_t ip = INADDR_ANY;
-    addr_.sin_port = port;  //fix
-    addr_.sin_addr.s_addr = ip; //fix
+    addr_.sin_port = htons(port);
+    addr_.sin_addr.s_addr = htonl(ip); 
 }
 
 InetAddress::InetAddress(std::string ip, uint16_t port)
--- NETLIB/InetAddress.h
+++ NETLIB/InetAddress.h
@@ -1,6 +1,7 @@
 #include <netinet/in.h>
 #include <string>
 
+extern const struct sockaddr* sockaddr_cast(const struct sockaddr_in* addr);
 
 class InetAddress
 {
@@ -12,9 +13,19 @@ public:
     }
     
     sa_family_t family() const { return addr_.sin_family; }
-    std::string toIP() const;
-    std::string toIpPort() const;
-    uint16_t toPort() const;
+    //std::string toIP() const;
+    //std::string toIpPort() const;
+    //uint16_t toPort() const;
+
+    const struct sockaddr* getSockAddr() const 
+    {
+        return sockaddr_cast(&addr_);
+    }
+
+    void setSockAddrInet(const struct sockaddr_in& addr)
+    {
+        addr_ = addr;
+    }
 
 private:
     struct sockaddr_in addr_;
