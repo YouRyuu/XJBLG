b8ce71316cec236964f5816b22c5dd28f3da40ce
youryuu <l9819y@gmail.com>
2022-02-28 16:27:12 +0800

    test

--- NETLIB/main.cpp
+++ NETLIB/main.cpp
@@ -2,17 +2,9 @@
 #include "Eventloop.h"
 #include <unistd.h>
 
-EventLoop* gloop;
-
-void print()
-{
-    std::cout<<"print func"<<std::endl;
-}
-
 int main()
 {
-    EventLoop loop;
-    gloop = &loop;
+    
     
     return 0;
 }
