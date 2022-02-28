bee730eea6a8301b81ffab4eecfb7553ec41321e
youryuu <l9819y@gmail.com>
2022-02-28 20:04:52 +0800

    test02

--- NETLIB/main.cpp
+++ NETLIB/main.cpp
@@ -3,8 +3,10 @@
 #include <arpa/inet.h>
 #include "InetAddress.h"
 #include "Socket.h"
+#include "Eventloop.h"
+#include "Acceptor.h"
 
-int main()
+void test01()
 {
     InetAddress listenaddr(8090);
     Socket listenfd(socket(AF_INET, SOCK_STREAM, 0));
@@ -19,5 +21,30 @@ int main()
         inet_ntop(AF_INET, &peerAddr.getSock().sin_addr, buff, sizeof buff), ntohs(peerAddr.getSock().sin_port));
         close(connfd);
     }
+}
+
+void newConnection(int sockfd, const InetAddress& peerAddr)
+{
+    InetAddress peerAddr_;
+    peerAddr_ = const_cast<InetAddress&>(peerAddr);
+    char buff[1024];
+    printf("newconn from %s, port %d\n", 
+        inet_ntop(AF_INET, &peerAddr_.getSock().sin_addr, buff, sizeof buff), ntohs(peerAddr_.getSock().sin_port));
+    close(sockfd);
+}
+
+void test02()
+{
+    InetAddress listenAddr(8090);
+    EventLoop loop;
+    Acceptor acceptor(&loop, listenAddr, true);
+    acceptor.setNewConnectionCallback(newConnection);
+    acceptor.listen();
+    loop.loop();
+}
+
+int main()
+{
+    test02();
     return 0;
 }
