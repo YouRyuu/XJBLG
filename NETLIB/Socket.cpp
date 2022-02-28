90cd28dfec653413b87a4708c62393370a40e721
youryuu <l9819y@gmail.com>
2022-02-28 16:22:52 +0800

    Socket

---
+++ NETLIB/Socket.cpp
@@ -0,0 +1,76 @@
+#include "Socket.h"
+#include "InetAddress.h"
+#include "SocketsOps.h"
+
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <iostream>
+
+Socket::~Socket()
+{
+    close_(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress& localAddr)
+{
+    bindOrDie(sockfd_, localAddr.getSockAddr());
+}
+
+void Socket::listen()
+{
+    listenOrDie(sockfd_);
+}
+
+int Socket::accept(InetAddress* peerAddr)
+{
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof addr);
+    int connfd = accept_(sockfd_, &addr);
+    if(connfd > 0)
+    {
+        peerAddr->setSockAddrInet(addr);
+    }
+    return connfd;
+}
+
+void Socket::shutdownWrite_()
+{
+    shutdownWrite(sockfd_);
+}
+
+void Socket::setReuseAddr(bool on)
+{
+  int optval = on ? 1 : 0;
+  ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR,
+               &optval, static_cast<socklen_t>(sizeof optval));
+  // FIXME CHECK
+}
+
+void Socket::setReusePort(bool on)
+{
+#ifdef SO_REUSEPORT
+  int optval = on ? 1 : 0;
+  int ret = setsockopt(sockfd_, SOL_SOCKET, SO_REUSEPORT,
+                         &optval, static_cast<socklen_t>(sizeof optval));
+  if (ret < 0 && on)
+  {
+    std::cout << "SO_REUSEPORT failed." << std::endl;
+    exit(1);
+  }
+#else
+  if (on)
+  {
+    std::cout << "SO_REUSEPORT is not supported." << std::endl;
+    exit(1);
+  }
+#endif
+}
+
+void Socket::setKeepAlive(bool on)
+{
+  int optval = on ? 1 : 0;
+  setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE,
+               &optval, static_cast<socklen_t>(sizeof optval));
+  // FIXME CHECK
+}
---
+++ NETLIB/Socket.h
@@ -0,0 +1,38 @@
+class InetAddress;
+struct tcp_info;
+
+class Socket
+{
+    public:
+        Socket(int sockfd)
+        :sockfd_(sockfd)
+        { }
+
+        ~Socket();
+
+        int fd() const 
+        {
+            return sockfd_;
+        }
+
+        //bool getTcpInfo(struct tco_info*) const;
+        
+        //bool getTcpInfoString(char* buf, int len) const;
+
+        void bindAddress(const InetAddress& localAddr);
+
+        void listen();
+
+        int accept(InetAddress* peerAddr);
+
+        void shutdownWrite_();
+
+        void setReuseAddr(bool on);
+
+        void setReusePort(bool on);
+
+        void setKeepAlive(bool on);
+
+    private:
+        const int sockfd_;
+};
