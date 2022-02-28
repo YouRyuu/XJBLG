1fb2c32dbc93c2a0298a02dd4a333cd9e57b08e2
youryuu <l9819y@gmail.com>
2022-02-28 17:07:02 +0800

    test

--- NETLIB/main.cpp
+++ NETLIB/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include "Eventloop.h"
+#include <stdio.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include "InetAddress.h"
+#include "Socket.h"
 
 int main()
 {
-    
-    
+    InetAddress listenaddr(8090);
+    Socket listenfd(socket(AF_INET, SOCK_STREAM, 0));
+    listenfd.bindAddress(listenaddr);
+    listenfd.listen();
+    for(;;)
+    {
+        char buff[1024];
+        InetAddress peerAddr;
+        int connfd = listenfd.accept(&peerAddr);
+        printf("conn from %s, port %d\n", 
+        inet_ntop(AF_INET, &peerAddr.getSock().sin_addr, buff, sizeof buff), ntohs(peerAddr.getSock().sin_port));
+        close(connfd);
+    }
     return 0;
 }
