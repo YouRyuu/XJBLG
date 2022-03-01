19b52b2d210eff55e1e546a62e6c8f8201b88ac9
youryuu <l9819y@gmail.com>
2022-03-01 18:05:34 +0800

    callbacks

---
+++ NETLIB/Callbacks.h
@@ -0,0 +1,26 @@
+#ifndef WS_CALLBACKS_H
+#define WS_CALLBACKS_H
+#include <memory>
+#include <functional>
+template<class T>
+inline T* getPointer(const std::shared_ptr<T>& ptr)
+{
+    return ptr.get();
+}
+
+template<class T>
+inline T* getPointer(const std::unique_ptr<T>& ptr)
+{
+    return ptr.get();
+}
+
+class TcpConnection;
+typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
+typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
+typedef std::function<void (const TcpConnectionPtr&)> CloseCallback;
+typedef std::function<void (const TcpConnectionPtr&)> WriteCompleteCallback;
+typedef std::function<void (const TcpConnectionPtr&, size_t)> HighWaterMarkCallback;
+typedef std::function<void (const TcpConnectionPtr&, char*, int)> MessageCallback;
+void defaultConnectionCallback(const TcpConnectionPtr& conn);
+void defaultMessageCallback(const TcpConnectionPtr& conn, char* buf, int);
+#endif
