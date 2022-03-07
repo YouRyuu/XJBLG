f63eb1e0e064c7bd5857beb2b78df0a2813d4b05
youryuu <l9819y@gmail.com>
2022-03-07 21:33:15 +0800

    ConditionJ

---
+++ NETLIB/Condition.h
@@ -0,0 +1,39 @@
+#ifndef WS_CONDITION_H
+#define WS_CONDITION_H
+
+#include "Mutex.h"
+
+class Condition
+{
+    private:
+        MutexLock& mutex_;
+        pthread_cond_t pcond_;
+    public:
+        explicit Condition(MutexLock& mutex):mutex_(mutex)
+        {
+            MCHECK(pthread_cond_init(&pcond_, NULL));
+        }
+
+        ~Condition()
+        {
+            MCHECK(pthread_cond_destroy(&pcond_));
+        }
+
+        void wait()
+        {
+            MCHECK(pthread_cond_wait(&pcond_, mutex_.get()));
+        }
+
+        void notify()
+        {
+            MCHECK(pthread_cond_signal(&pcond_));
+        }
+
+        void notifyAll()
+        {
+            MCHECK(pthread_cond_broadcast(&pcond_));
+        }
+
+};
+
+#endif
