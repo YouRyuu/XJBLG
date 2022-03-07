694d6c6383fa803939231b00f2fa3b71e0c9eb18
youryuu <l9819y@gmail.com>
2022-03-07 21:33:35 +0800

    CountDownLatch

---
+++ NETLIB/CountDownLatch.h
@@ -0,0 +1,23 @@
+#ifndef WS_COUNTDOWNLATCH_H
+#define WS_COUNTDOWNLATCH_H
+
+#include "Mutex.h"
+#include "Condition.h"
+
+class CountDownLatch
+{
+    /*
+    countdowklatch的作用在于保证创建的线程的func运行之后，调用方的start函数再返回
+    */
+    private:
+        MutexLock mutex_;
+        Condition condition_;
+        int count_;
+    public:
+        explicit CountDownLatch(int count);
+        void wait();
+        void countDown();
+        int getCount();
+};
+
+#endif
