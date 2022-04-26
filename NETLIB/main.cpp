8c32cc0ff5011eb423b581582df67e59a021745e
youryuu <l9819y@gmail.com>
2022-04-26 15:21:20 +0800

    server

--- NETLIB/main.cpp
+++ NETLIB/main.cpp
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
 #include <arpa/inet.h>
 #include <string>
-#include "InetAddress.h"
-#include "Socket.h"
-#include "Eventloop.h"
-#include "Acceptor.h"
-#include "TcpConnection.h"
-#include "TcpServer.h"
-#include "Buffer.h"
-#include "StringPiece.h"
-#include "ThreadPool.h"
-#include "CurrentThread.h"
+#include "net/InetAddress.h"
+#include "net/Socket.h"
+#include "net/Eventloop.h"
+#include "net/Acceptor.h"
+#include "net/TcpConnection.h"
+#include "net/TcpServer.h"
+#include "net/Buffer.h"
+#include "net/StringPiece.h"
+#include "net/ThreadPool.h"
+#include "net/CurrentThread.h"
+#include "application/http/HttpResponse.h"
+#include "application/http/HttpServer.h"
+#include "application/http/HttpRequest.h"
+#include "application/chat/ChatServer.h"
 
-
-char *s = "HTTP/1.1 200 OK\r\nContent-Length: 91\r\nConnecion: Keep-Alive\r\nContent-Type: text/html\r\nServer: Muduo\r\n\r\n<html><head><title>This is title</title></head><body><h1>Hello</h1>Now is xxx</body></html>";
-
+extern char favicon[555];
+////int filefd = open("photo2.jpg", O_RDONLY);
+//Buffer photo;
 
 void onMessage(const TcpConnectionPtr &conn, Buffer *buf, int size)
 {
-    printf("onMessage:tid=%d, recv %d bytes from [%s]:%s\n", CurrentThread::tid(),  size, conn->name().c_str(), buf->retrieveAllAsString().c_str());
-    conn->send(s);
+    printf("onMessage:tid=%d, recv %d bytes from [%s]:%s\n", CurrentThread::tid(), size, conn->name().c_str(), buf->retrieveAllAsString().c_str());
+    // conn->send(s);
 }
 
 void onConnection(const TcpConnectionPtr &conn)
@@ -29,8 +34,6 @@ void onConnection(const TcpConnectionPtr &conn)
     if (conn->connected())
     {
         printf("onConnection:tid=%d, new conn [%s]\n", CurrentThread::tid(), conn->name().c_str());
-        //conn->send("HTTP/1.1 400 Bad Request\r\n\r\n");
-        // conn->shutdown();
     }
     else
     {
@@ -38,11 +41,7 @@ void onConnection(const TcpConnectionPtr &conn)
     }
 }
 
-void test03()
-{
-}
-
-int main()
+void test05()
 {
     printf("main():tid=%d\n", getpid());
     InetAddress listenAddr(9898);
@@ -50,8 +49,697 @@ int main()
     TcpServer server(&loop, listenAddr, "myserver");
     server.setConnectionCallback(onConnection);
     server.setMessageCallback(onMessage);
-    server.setThreadNum(0);
+    server.setThreadNum(3);
     server.start();
     loop.loop();
+}
+std::string name;
+
+void onRequest(const HttpRequest &req, HttpResponse *resp)
+{
+    int method = req.getMethod();
+    //std::cout << req.methodString() << std::endl;
+    std::string path = req.getPath();
+    //std::cout<<path<<std::endl;
+    // if(filefd < 0)
+    // {
+    //     std::cout<<"open photo1.jpg error"<<std::endl;
+    //     exit(1);
+    // }
+    if (path == "/" && method == HttpRequest::Method::GET)
+    {
+        name = "guest";
+        resp->setStatusCode(HttpResponse::k200ok);
+        resp->setStatusMessage("OK");
+        resp->setContentType("text/html");
+        resp->addHeader("Server", "MyServer");
+        resp->setBody((std::string("<!DOCTYPE html>"
+                                   "<html lang=\"en\"> "
+                                   "<head>"
+                                   " <meta charset=\"UTF-8\">"
+                                   "<title>Title</title>"
+                                   "</head>"
+                                   "<body>"
+                                   "hello, ") +
+                       name +
+                       std::string("<form method=\"post\">"
+                                   "       <div >"
+                                   " <input type=\"text\" placeholder=\"请输入手机号\" name=\"tel\"/>"
+                                   "  <input type=\"password\" placeholder=\"请输入密码\" name=\"password\"/>"
+                                   "      </div>"
+                                   "  <div class=\"one\">"
+                                   "    <table>"
+                                   "    <tr>"
+                                   "   <td><button type=\"submit\" >登录</button> </td>"
+                                   "    <td><button>注册</button></td>"
+                                   "     </tr>"
+                                   "    </table>"
+                                   " </div>"
+                                   "</body>"
+                                   "</html>"))
+                          .c_str());
+    }
+    else if (path == "/" && method == HttpRequest::Method::POST)
+    {
+        //std::cout<<"in path=/ and method=post:"<<method<<std::endl;
+        //读取POST的body数据,格式:pam1=value1&pam2=value2...
+        name = req.getBody();
+        //std::cout << name << std::endl;
+        resp->setStatusCode(HttpResponse::k200ok);
+        resp->setStatusMessage("OK");
+        resp->setContentType("text/html");
+        resp->addHeader("Server", "MyServer");
+        resp->setBody((std::string("<!DOCTYPE html>"
+                                   "<html lang=\"en\"> "
+                                   "<head>"
+                                   " <meta charset=\"UTF-8\">"
+                                   "<title>Title</title>"
+                                   "</head>"
+                                   "<body>"
+                                   "hello, ") +
+                       name)
+                          .c_str() +
+                      std::string("</body>"));
+    }
+    else
+    {
+        //int filefd = open("photo2.jpg", O_RDONLY);
+        //Buffer photo;
+        //if(filefd < 0)
+        //{
+       //     std::cout<<"filefd error"<<std::endl;
+       //     exit(1);
+        //}
+        //photo.bufReadFd(filefd);
+        
+        resp->setStatusCode(HttpResponse::k200ok);
+        resp->setStatusMessage("OK");
+        resp->setContentType("text/html");
+        //resp->setContentType("image/png");
+        resp->addHeader("Server", "Muduo");
+        resp->setBody("<html><head><title>This is title</title></head>"
+                     "<body><h1>Hello</h1><img src=\"img.png\"></body></html>");
+        //resp->setBody(photo.retrieveAllAsString());
+        //close(filefd);
+    }
+}
+void onRequest2(const HttpRequest &req, HttpResponse *resp)
+{
+    //std::cout << "Header:" << req.methodString() << " " << req.getPath() << std::endl;
+    // const std::map<std::string, std::string> &headers = req.getHeaders();
+    // for (const auto &item : headers)
+    // {
+    //     std::cout << item.first << ":" << item.second << std::endl;
+    // }
+    resp->setStatusCode(HttpResponse::k200ok);
+    resp->setStatusMessage("OK");
+    resp->setContentType("text/html");
+    //resp->setContentType("image/png");
+    resp->addHeader("Server", "Muduo");
+     resp->setBody("<html><head><title>This is title</title></head>"
+                  "<body><h1>Hello</h1>Now is xxx</body></html>");
+}
+
+void testHttp()
+{
+    int numThreads = 4;
+    EventLoop loop;
+    HttpServer server(&loop, InetAddress(9898), "dummy");
+    server.setHttpCallback(onRequest);
+    server.setThreadNum(numThreads);
+    server.start();
+    loop.loop();
+}
+
+void testChat()
+{
+    EventLoop loop;
+    ChatServer server(&loop, InetAddress(6666), "ChatServer");
+    server.start();
+    loop.loop();
+}
+
+int main(int argc, char *argv[])
+{
+    testChat();
+    //testHttp();
     return 0;
 }
+
+char favicon[555] = {
+    '\x89',
+    'P',
+    'N',
+    'G',
+    '\xD',
+    '\xA',
+    '\x1A',
+    '\xA',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\xD',
+    'I',
+    'H',
+    'D',
+    'R',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\x10',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\x10',
+    '\x8',
+    '\x6',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\x1F',
+    '\xF3',
+    '\xFF',
+    'a',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\x19',
+    't',
+    'E',
+    'X',
+    't',
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    '\x0',
+    'A',
+    'd',
+    'o',
+    'b',
+    'e',
+    '\x20',
+    'I',
+    'm',
+    'a',
+    'g',
+    'e',
+    'R',
+    'e',
+    'a',
+    'd',
+    'y',
+    'q',
+    '\xC9',
+    'e',
+    '\x3C',
+    '\x0',
+    '\x0',
+    '\x1',
+    '\xCD',
+    'I',
+    'D',
+    'A',
+    'T',
+    'x',
+    '\xDA',
+    '\x94',
+    '\x93',
+    '9',
+    'H',
+    '\x3',
+    'A',
+    '\x14',
+    '\x86',
+    '\xFF',
+    '\x5D',
+    'b',
+    '\xA7',
+    '\x4',
+    'R',
+    '\xC4',
+    'm',
+    '\x22',
+    '\x1E',
+    '\xA0',
+    'F',
+    '\x24',
+    '\x8',
+    '\x16',
+    '\x16',
+    'v',
+    '\xA',
+    '6',
+    '\xBA',
+    'J',
+    '\x9A',
+    '\x80',
+    '\x8',
+    'A',
+    '\xB4',
+    'q',
+    '\x85',
+    'X',
+    '\x89',
+    'G',
+    '\xB0',
+    'I',
+    '\xA9',
+    'Q',
+    '\x24',
+    '\xCD',
+    '\xA6',
+    '\x8',
+    '\xA4',
+    'H',
+    'c',
+    '\x91',
+    'B',
+    '\xB',
+    '\xAF',
+    'V',
+    '\xC1',
+    'F',
+    '\xB4',
+    '\x15',
+    '\xCF',
+    '\x22',
+    'X',
+    '\x98',
+    '\xB',
+    'T',
+    'H',
+    '\x8A',
+    'd',
+    '\x93',
+    '\x8D',
+    '\xFB',
+    'F',
+    'g',
+    '\xC9',
+    '\x1A',
+    '\x14',
+    '\x7D',
+    '\xF0',
+    'f',
+    'v',
+    'f',
+    '\xDF',
+    '\x7C',
+    '\xEF',
+    '\xE7',
+    'g',
+    'F',
+    '\xA8',
+    '\xD5',
+    'j',
+    'H',
+    '\x24',
+    '\x12',
+    '\x2A',
+    '\x0',
+    '\x5',
+    '\xBF',
+    'G',
+    '\xD4',
+    '\xEF',
+    '\xF7',
+    '\x2F',
+    '6',
+    '\xEC',
+    '\x12',
+    '\x20',
+    '\x1E',
+    '\x8F',
+    '\xD7',
+    '\xAA',
+    '\xD5',
+    '\xEA',
+    '\xAF',
+    'I',
+    '5',
+    'F',
+    '\xAA',
+    'T',
+    '\x5F',
+    '\x9F',
+    '\x22',
+    'A',
+    '\x2A',
+    '\x95',
+    '\xA',
+    '\x83',
+    '\xE5',
+    'r',
+    '9',
+    'd',
+    '\xB3',
+    'Y',
+    '\x96',
+    '\x99',
+    'L',
+    '\x6',
+    '\xE9',
+    't',
+    '\x9A',
+    '\x25',
+    '\x85',
+    '\x2C',
+    '\xCB',
+    'T',
+    '\xA7',
+    '\xC4',
+    'b',
+    '1',
+    '\xB5',
+    '\x5E',
+    '\x0',
+    '\x3',
+    'h',
+    '\x9A',
+    '\xC6',
+    '\x16',
+    '\x82',
+    '\x20',
+    'X',
+    'R',
+    '\x14',
+    'E',
+    '6',
+    'S',
+    '\x94',
+    '\xCB',
+    'e',
+    'x',
+    '\xBD',
+    '\x5E',
+    '\xAA',
+    'U',
+    'T',
+    '\x23',
+    'L',
+    '\xC0',
+    '\xE0',
+    '\xE2',
+    '\xC1',
+    '\x8F',
+    '\x0',
+    '\x9E',
+    '\xBC',
+    '\x9',
+    'A',
+    '\x7C',
+    '\x3E',
+    '\x1F',
+    '\x83',
+    'D',
+    '\x22',
+    '\x11',
+    '\xD5',
+    'T',
+    '\x40',
+    '\x3F',
+    '8',
+    '\x80',
+    'w',
+    '\xE5',
+    '3',
+    '\x7',
+    '\xB8',
+    '\x5C',
+    '\x2E',
+    'H',
+    '\x92',
+    '\x4',
+    '\x87',
+    '\xC3',
+    '\x81',
+    '\x40',
+    '\x20',
+    '\x40',
+    'g',
+    '\x98',
+    '\xE9',
+    '6',
+    '\x1A',
+    '\xA6',
+    'g',
+    '\x15',
+    '\x4',
+    '\xE3',
+    '\xD7',
+    '\xC8',
+    '\xBD',
+    '\x15',
+    '\xE1',
+    'i',
+    '\xB7',
+    'C',
+    '\xAB',
+    '\xEA',
+    'x',
+    '\x2F',
+    'j',
+    'X',
+    '\x92',
+    '\xBB',
+    '\x18',
+    '\x20',
+    '\x9F',
+    '\xCF',
+    '3',
+    '\xC3',
+    '\xB8',
+    '\xE9',
+    'N',
+    '\xA7',
+    '\xD3',
+    'l',
+    'J',
+    '\x0',
+    'i',
+    '6',
+    '\x7C',
+    '\x8E',
+    '\xE1',
+    '\xFE',
+    'V',
+    '\x84',
+    '\xE7',
+    '\x3C',
+    '\x9F',
+    'r',
+    '\x2B',
+    '\x3A',
+    'B',
+    '\x7B',
+    '7',
+    'f',
+    'w',
+    '\xAE',
+    '\x8E',
+    '\xE',
+    '\xF3',
+    '\xBD',
+    'R',
+    '\xA9',
+    'd',
+    '\x2',
+    'B',
+    '\xAF',
+    '\x85',
+    '2',
+    'f',
+    'F',
+    '\xBA',
+    '\xC',
+    '\xD9',
+    '\x9F',
+    '\x1D',
+    '\x9A',
+    'l',
+    '\x22',
+    '\xE6',
+    '\xC7',
+    '\x3A',
+    '\x2C',
+    '\x80',
+    '\xEF',
+    '\xC1',
+    '\x15',
+    '\x90',
+    '\x7',
+    '\x93',
+    '\xA2',
+    '\x28',
+    '\xA0',
+    'S',
+    'j',
+    '\xB1',
+    '\xB8',
+    '\xDF',
+    '\x29',
+    '5',
+    'C',
+    '\xE',
+    '\x3F',
+    'X',
+    '\xFC',
+    '\x98',
+    '\xDA',
+    'y',
+    'j',
+    'P',
+    '\x40',
+    '\x0',
+    '\x87',
+    '\xAE',
+    '\x1B',
+    '\x17',
+    'B',
+    '\xB4',
+    '\x3A',
+    '\x3F',
+    '\xBE',
+    'y',
+    '\xC7',
+    '\xA',
+    '\x26',
+    '\xB6',
+    '\xEE',
+    '\xD9',
+    '\x9A',
+    '\x60',
+    '\x14',
+    '\x93',
+    '\xDB',
+    '\x8F',
+    '\xD',
+    '\xA',
+    '\x2E',
+    '\xE9',
+    '\x23',
+    '\x95',
+    '\x29',
+    'X',
+    '\x0',
+    '\x27',
+    '\xEB',
+    'n',
+    'V',
+    'p',
+    '\xBC',
+    '\xD6',
+    '\xCB',
+    '\xD6',
+    'G',
+    '\xAB',
+    '\x3D',
+    'l',
+    '\x7D',
+    '\xB8',
+    '\xD2',
+    '\xDD',
+    '\xA0',
+    '\x60',
+    '\x83',
+    '\xBA',
+    '\xEF',
+    '\x5F',
+    '\xA4',
+    '\xEA',
+    '\xCC',
+    '\x2',
+    'N',
+    '\xAE',
+    '\x5E',
+    'p',
+    '\x1A',
+    '\xEC',
+    '\xB3',
+    '\x40',
+    '9',
+    '\xAC',
+    '\xFE',
+    '\xF2',
+    '\x91',
+    '\x89',
+    'g',
+    '\x91',
+    '\x85',
+    '\x21',
+    '\xA8',
+    '\x87',
+    '\xB7',
+    'X',
+    '\x7E',
+    '\x7E',
+    '\x85',
+    '\xBB',
+    '\xCD',
+    'N',
+    'N',
+    'b',
+    't',
+    '\x40',
+    '\xFA',
+    '\x93',
+    '\x89',
+    '\xEC',
+    '\x1E',
+    '\xEC',
+    '\x86',
+    '\x2',
+    'H',
+    '\x26',
+    '\x93',
+    '\xD0',
+    'u',
+    '\x1D',
+    '\x7F',
+    '\x9',
+    '2',
+    '\x95',
+    '\xBF',
+    '\x1F',
+    '\xDB',
+    '\xD7',
+    'c',
+    '\x8A',
+    '\x1A',
+    '\xF7',
+    '\x5C',
+    '\xC1',
+    '\xFF',
+    '\x22',
+    'J',
+    '\xC3',
+    '\x87',
+    '\x0',
+    '\x3',
+    '\x0',
+    'K',
+    '\xBB',
+    '\xF8',
+    '\xD6',
+    '\x2A',
+    'v',
+    '\x98',
+    'I',
+    '\x0',
+    '\x0',
+    '\x0',
+    '\x0',
+    'I',
+    'E',
+    'N',
+    'D',
+    '\xAE',
+    'B',
+    '\x60',
+    '\x82',
+};
