6f070942850b0039a8b5a63b302bc122e5788c8d
youryuu <l9819y@gmail.com>
2022-04-30 22:46:14 +0800

    完善解析序列化字段

--- NETLIB/function.h
+++ NETLIB/function.h
@@ -1,69 +1,104 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
 #include <string>
 #include <algorithm>
-bool parseJson(const char *begin, const char *end)
+#include <unordered_map>
+class JsonItem
 {
-    // json解析器
-    /*
-     *{后必定接一个"
-     *{"code":"1000","sender":"1234","recver":"1235","time":"2022-02-02 14:05:34","body":"hello"}
-     *{"code":"1000","sender":"1234","recver":"1235","time":"2022-02-02 14:05:34","body":[{"1236":"username"},{"1237":"nameuser"}]}
-     *{"code":"1000","sender":"1234","recver":"1235","time":"2022-02-02 14:05:34","body":["111","222"]}
-     */
-    bool ret = true;
-    const char *start = begin;
-    while (start != end && ret)
+public:
+    bool parseJson(const char *begin, const char *end)
     {
-        if (*start == '{' || *start == ',')
+        // json解析器
+        bool ret = true;
+        const char *start = begin;
+        while (start != end && ret)
         {
-            if (*(start + 1) != '"')
-                ret = false;
-            else ret = true;
-            if (ret)
+            if (*start == '{' || *start == ',') //逗号和{后必跟冒号
             {
-                start = start + 2;
-                const char *keyEnd = std::find(start, end, '"');
-                if (keyEnd != end)      //取键
+                if (*(start + 1) != '"')
+                    ret = false;
+                else
+                    ret = true;
+                if (ret)
                 {
-                    std::string key;
-                    key.assign(start, keyEnd);
-                    start = keyEnd + 1;
-                    if (*start != ':')
-                        ret = false;
-                    if (ret)        //取值  
+                    start = start + 2;                               //移动到key的第一个字母
+                    const char *keyEnd = std::find(start, end, '"'); //找到key的结束位置
+                    if (keyEnd != end)                               //取键
                     {
-                        start = start + 1;
-                        if (*start == '"')      //是字符串
+                        std::string key;
+                        key.assign(start, keyEnd);
+                        start = keyEnd + 1; // start到了冒号这里
+                        if (*start != ':')
+                            ret = false;
+                        if (ret) //取值
                         {
                             start = start + 1;
-                            const char *valueEnd = std::find(start, end, '"');
-                            if (valueEnd != end)
+                            if (*start == '"') //是字符串
                             {
-                                std::string value;
-                                value.assign(start, valueEnd);
-                                start = valueEnd + 1;       //一个键值对已经取完了
-                                printf("Key:%s, Value:%s\n", key.c_str(), value.c_str());
+                                start = start + 1; //到value的第一个字母
+                                const char *valueEnd = std::find(start, end, '"');
+                                if (valueEnd != end)
+                                {
+                                    std::string value;
+                                    value.assign(start, valueEnd);
+                                    start = valueEnd + 1; //一个键值对已经取完了
+                                    jsonItem[key] = value;
+                                    // printf("Key:%s, Value:%s\n", key.c_str(), value.c_str());
+                                }
+                                else
+                                    ret = false;
                             }
-                            else
+                            else if (*start == '{')        //是列表
+                            {
+            //{"code":"1000","sender":"1234","recver":"1235","time":"2022-02-02 14:05:34","body":{"1236":"username","1237":"nameuser"}}
+                                const char *valueEnd = std::find(start, end, '}');
+                                if(valueEnd != end)
+                                {
+                                    std::string value;
+                                    value.assign(start, valueEnd+1);
+                                    start = valueEnd+1;
+                                    jsonItem[key] = value;
+                                }
+                                else
+                                    ret = false;
+                            }
+                            else //是数字
                                 ret = false;
                         }
-                        else if (*start == '[')
-                        {
-                        }
-                        else
-                            ret = false;
                     }
+                    else
+                        ret = false;
                 }
-                else
-                    ret = false;
             }
-        }
-        else if(*start == '}')
+            else if (*start == '}')
             {
                 ret = true;
                 start = start + 1;
             }
-        else
+            else
+                ret = false;
+        }
+        if (*(start - 1) != '}')
             ret = false;
+        if (!ret)
+            jsonItem.clear();
+        return ret;
     }
-    return ret;
-}
+    std::unordered_map<std::string, std::string> jsonItem;
+};
+
+// std::string getNowTime()
+// {
+//     time_t now = time(0);
+//     tm *ltm = localtime(&now);
+//     std::string year = std::to_string(1900 + ltm->tm_year) + "/";
+//     std::string month = std::to_string(1 + ltm->tm_mon) + "/";
+//     std::string day = std::to_string(ltm->tm_mday) + " ";
+//     std::string hour = std::to_string(ltm->tm_hour) + ":";
+//     std::string mins = std::to_string(ltm->tm_min) + ":";
+//     std::string sec = std::to_string(ltm->tm_sec);
+//     return year + month + day + hour + mins + sec;
+// }
+
+#endif
